551bba4d7b20b28ad2c686db577b9a2229ce8f0d
emiir14 <emirderespaldo@gmail.com>
2020-04-14 19:44:46 -0300

    few changes everywhere

--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -3,15 +3,21 @@ import { Injectable } from '@angular/core';
 export const darkTheme = {
   'primary-color': '#455363',
   'background-color': '#1C1C28',
+  'background-color2': '#555770',
   'text-color': '#88899E',
   'text-shadow': 'none',
+  'img-source': 'url(https://i.imgur.com/zpTr8SG.png)',
+  'img-darkening': 'brightness(70%)',
 };
 
 export const lightTheme = {
   'primary-color': '#fff',
-  'background-color': '#eeeeee',
+  'background-color': '#EBEBF0',
+  'background-color2': '#FAFAFC',
   'text-color': '#333',
   'text-shadow': '4px 4px 4px #aaa',
+  'img-source': 'url(https://i.imgur.com/gEE0Mge.png)',
+  'img-darkening': 'brightness(100%)',
 };
 
 @Injectable({ providedIn: 'root' })
